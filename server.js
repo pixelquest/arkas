e91a8c7d426a0f934561e785ee3d8093a272a129
pixelquest <pixelquest@gmail.com>
2025-04-02 11:50:47 +0530

    update port info

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ require("./app/routes/location.routes")(app);
 
 // set port, listen for requests
 // const PORT = process.env.PORT || 8080;
-const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 8080) : 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
