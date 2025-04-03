abc518a0156ef5c1204f1a3dabcf5540fe5ed838
pixelquest <pixelquest@gmail.com>
2025-04-03 15:20:26 +0530

    updated code

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ const allowedOrigins = ['http://localhost:4200', 'http://arkasfacilities.com', '
 app.use(
   cors({
     credentials: true,
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); // Allow the request
-      } else {
-        callback(new Error('Not allowed by CORS')); // Block the request
-      }
-    },
+    // origin: (origin, callback) => {
+    //   if (!origin || allowedOrigins.includes(origin)) {
+    //     callback(null, true); // Allow the request
+    //   } else {
+    //     callback(new Error('Not allowed by CORS')); // Block the request
+    //   }
+    // },
   })
 );
 
