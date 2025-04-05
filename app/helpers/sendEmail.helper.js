9238db1ca07c45074696f0d8b688488bb891b8bb
pixelquest <pixelquest@gmail.com>
2025-04-05 14:02:32 +0530

    updated code

--- app/helpers/sendEmail.helper.js
+++ app/helpers/sendEmail.helper.js
@@ -3,14 +3,23 @@ const nodemailer = require("nodemailer");
 const sendEmailHelper = async (email, subject, text) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: process.env.HOST,
-            service: process.env.SERVICE,
-            port: 587,
-            secure: true,
+            // host: process.env.HOST,
+            // service: process.env.SERVICE,
+            // port: 587,
+            // secure: true,
+            // auth: {
+            //     // user: process.env.USER,
+            //     // pass: process.env.PASS,
+            //     user: "lavanyachiluveri1991@gmail.com",
+            //     pass: "Lucky$1491#",
+            // },
+
+            service: "gmail", // Example for using Gmail
             auth: {
-                user: process.env.USER,
-                pass: process.env.PASS,
+                user: "lavanyachiluveri1991@gmail.com",
+                pass: "Lucky$1491#",
             },
+
         });
 
         await transporter.sendMail({
